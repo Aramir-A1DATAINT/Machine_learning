0bb14babf47b7e37f144586e1a011b479fadb399
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2021-11-15 11:16:33 +0900

    bash file to load data and Data

---
+++ CollaborativeFilter_recommend/envNdata.sh
@@ -0,0 +1,4 @@
+pip install scikit-surprise
+wget https://raw.githubusercontent.com/towardsai/tutorials/master/recommendation_system_tutorial/movie_titles.csv
+wget https://raw.githubusercontent.com/towardsai/tutorials/master/recommendation_system_tutorial/new_features.csv
+wget https://datasets.towardsai.net/combined_data_4.txt
